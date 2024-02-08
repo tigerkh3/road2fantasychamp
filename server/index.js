49ea7d1b703e0c8254105ff5413c082c0eb6b636
Tiger Hong <tigerhong@Tigers-MBP.lan>
2024-02-07 19:37:03 -0800

    deployment-9
    removed development bundle.js as it was affecting production.

--- server/index.js
+++ server/index.js
@@ -11,10 +11,6 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
